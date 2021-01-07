832415c259b3198fff4d8b5faaf9c57439607904
JQ <2016quezadaj@gmail.com>
2021-01-07 13:48:21 -0800

    Created components, css, and pages folder

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <div className="description">
+                <div className="title">
+                    Qutefit
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
