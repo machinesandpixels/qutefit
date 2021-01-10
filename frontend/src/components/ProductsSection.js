4f31b0c47608667a9ffccc4faaccad526fe77f19
JQ <2016quezadaj@gmail.com>
2021-01-09 23:27:36 -0800

    Created ProductsSection component and imported it to the HomePage component

---
+++ frontend/src/components/ProductsSection.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProductsSection = () => {
+    return (
+        <div className="products">
+            <div className="description">
+                <h2>Products</h2>
+                <div className="cards">
+                    <div className="card">
+                        <h2>Title</h2>
+                        <div className="icon">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsSection;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ProductsSection from '../components/ProductsSection';
 
 const HomePage = () => {
     return (
         <div>
             <AboutSection />
+            <ProductsSection />
         </div>
     )
 }
